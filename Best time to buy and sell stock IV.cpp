5371985d0030e888550038787154b3c1bf79e07a
kunal_cpp <kunaliesc@gmail.com>
2021-11-02 19:56:01 +0530

    Update Best time to buy and sell stock IV.cpp

--- Best time to buy and sell stock IV.cpp
+++ Best time to buy and sell stock IV.cpp
@@ -1,51 +1,58 @@
 /*
 Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iv/
 
-Best Time to Buy and Sell Stock IV
+Best Time to Buy and Sell Stock with Cooldown
 
-You are given an integer array prices where prices[i] is the price of a given stock on the ith day, and an integer k.
-Find the maximum profit you can achieve. You may complete at most k transactions.
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+Find the maximum profit you can achieve. You may complete as many transactions as you like (i.e., buy one and sell 
+one share of the stock multiple times) with the following restrictions:
 
+After you sell your stock, you cannot buy stock on the next day (i.e., cooldown one day).
 Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
 
 Example 1:
-Input: k = 2, prices = [2,4,1]
-Output: 2
-Explanation: Buy on day 1 (price = 2) and sell on day 2 (price = 4), profit = 4-2 = 2.
+Input: prices = [1,2,3,0,2]
+Output: 3
+Explanation: transactions = [buy, sell, cooldown, buy, sell]
 
 Example 2:
-Input: k = 2, prices = [3,2,6,5,0,3]
-Output: 7
-Explanation: Buy on day 2 (price = 2) and sell on day 3 (price = 6), profit = 6-2 = 4. Then buy on day 5 (price = 0) and sell 
-on day 6 (price = 3), profit = 3-0 = 3.
+Input: prices = [1]
+Output: 0
  
+
 Constraints:
-0 <= k <= 100
-0 <= prices.length <= 1000
+1 <= prices.length <= 5000
 0 <= prices[i] <= 1000
 */
 
-
 class Solution {
 public:
-    int maxProfit(int k, vector<int>& prices) {
+    int maxProfit(vector<int>& prices) {
         
         int n = prices.size();
-        if(n == 0 || n == 1) return 0;
+        int obsp = -prices[0];
+        int ossp = 0;
+        int ocsp = 0;
         
-        vector<vector<int>> dp(k+1, vector<int>(n, 0));
-        
-        for(int i = 1; i <= k; i++){                   
+        for(int i = 1; i < n; i++){
             
-            int maxi = INT_MIN;
-            for(int j = 1; j < n; j++){                 
-                                  
-                maxi = max(maxi, dp[i-1][j-1] - prices[j-1]);
-                
-                dp[i][j] = max(maxi + prices[j], dp[i][j-1]);
-            }     
+            int nbsp = 0;
+            int nssp = 0;
+            int ncsp = 0;
+            if(ocsp - prices[i] > obsp) nbsp = ocsp - prices[i];
+            else nbsp = obsp;
+            
+            if(obsp + prices[i] > ossp) nssp = prices[i] + obsp;
+            else nssp = ossp;
+            
+            if(ossp > ocsp) ncsp = ossp;
+            else ncsp = ocsp;
+    
+            obsp = nbsp;
+            ossp = nssp;
+            ocsp = ncsp;
         }
-    return dp[k][n-1];
+       return ossp;
     }
 };
 
