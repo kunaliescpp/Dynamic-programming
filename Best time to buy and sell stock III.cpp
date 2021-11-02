d1807d695ab2bfee498174db5cfa74400db68ba6
kunal_cpp <kunaliesc@gmail.com>
2021-11-02 20:09:59 +0530

    Update Best time to buy and sell stock III.cpp

--- Best time to buy and sell stock III.cpp
+++ Best time to buy and sell stock III.cpp
@@ -1,78 +1,68 @@
 /*
-Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
+Problem Link: leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
 
-Best Time to Buy and Sell Stock III
+Best Time to Buy and Sell Stock with transaction fee (infinite transactions allowed)
 
-You are given an array prices where prices[i] is the price of a given stock on the ith day.
-Find the maximum profit you can achieve. You may complete at most two transactions.
+You are given an array prices where prices[i] is the price of a given stock on the ith day, and an integer fee representing 
+a transaction fee.
+Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction
+fee for each transaction.
+
 Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
 
 Example 1:
-Input: prices = [3,3,5,0,0,3,1,4]
-Output: 6
-Explanation: Buy on day 4 (price = 0) and sell on day 6 (price = 3), profit = 3-0 = 3. Then buy on day 7 (price = 1) and
-sell on day 8 (price = 4), profit = 4-1 = 3.
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
 
 Example 2:
-Input: prices = [1,2,3,4,5]
-Output: 4
-Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
-
-Note that you cannot buy on day 1, buy on day 2 and sell them later, as you are engaging multiple transactions at the same 
-time. You must sell before buying again.
-
-Example 3:
-Input: prices = [7,6,4,3,1]
-Output: 0
-Explanation: In this case, no transaction is done, i.e. max profit = 0.
-
-Example 4:
-Input: prices = [1]
-Output: 0
+Input: prices = [1,3,7,5,10,3], fee = 3
+Output: 6
  
 
 Constraints:
-1 <= prices.length <= 10^5
-0 <= prices[i] <= 10^5
+1 <= prices.length <= 5 * 10^4
+1 <= prices[i] < 5 * 10^4
+0 <= fee < 5 * 10^4
 */
-
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-       
+    int maxProfit(vector<int>& prices, int fee) {
+        
         int n = prices.size();
+        //loan leke stock kharidenge par hmpe loan ke sath 1 stock hoga...
+        //2nd din tabhi bekunga jab 2nd din stock bek ke ,3rd day stock mje me khareeed saku
         
-        int mpist = 0;                      //max profit - if sold today (is mandatory)
-        int mini = prices[0];              
-        int *dpl = new int [n];              // dp of max profit - if sold upto today 
-        dpl[0] = 0;
+        int obsp = -prices[0];                               // old brought stock price
+        int ossp = 0;                                        // old sell stock price
         for(int i = 1; i < n; i++){
+           
+            int nbsp = 0;                                    //new brought stock price
+            int nssp = 0;                                    //new sell stock price
             
-            mini = min(mini, prices[i]);
-            mpist = prices[i] - mini;
-            
-            dpl[i] = max(mpist, dpl[i-1]);
-        }
-        
-        int mpibt = 0;                      // max profit - if buy upto today(is mandatory)
-        int maxi = prices[n-1];
-        int *dpr = new int [n];
-        dpr[n-1] = 0;
-        for(int i = n-2; i >= 0; i--){
-            
-            maxi = max(maxi, prices[i]);
-            mpibt = maxi - prices[i];
-            
-            dpr[i] = max(mpibt, dpr[i+1]);
-        }
+            if(ossp - prices[i] > obsp){
+                nbsp = ossp - prices[i];
+            } else {
+               nbsp = obsp;
+            }
+           
+            if(obsp + prices[i] - fee > ossp){
+                nssp = obsp + prices[i] - fee;
+            } else {
+                nssp = ossp;
+            }
     
-        int op = 0;
-        for(int i = 0; i < n; i++){
-            op = max(op, dpl[i] + dpr[i]);
-        }
-        
-      return op;
+            obsp = nbsp;
+            ossp = nssp;
+        }  
+     
+     return ossp;
     }
 };
 
