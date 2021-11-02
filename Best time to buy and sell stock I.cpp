9b911ac1db047b56bdb276dacdc4e79b72c54ff6
kunal_cpp <kunaliesc@gmail.com>
2021-11-02 19:57:22 +0530

    Update Best time to buy and sell stock I.cpp

--- Best time to buy and sell stock I.cpp
+++ Best time to buy and sell stock I.cpp
@@ -4,10 +4,9 @@ Problem Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 Best Time to Buy and Sell Stock
 
 You are given an array prices where prices[i] is the price of a given stock on the ith day.
-
 You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future
-to sell that stock.
-Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+to sell that stock. Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit,
+return 0.
 
 Example 1:
 Input: prices = [7,1,5,3,6,4]
