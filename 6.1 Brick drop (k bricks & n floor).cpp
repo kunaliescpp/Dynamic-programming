848a0b7aa75468f9cd560e6dc2abad0c82948c1d
kunal_cpp <kunaliesc@gmail.com>
2022-07-15 02:00:38 +1300

    Update 6.1 Brick drop (k bricks & n floor).cpp

--- 6.1 Brick drop (k bricks & n floor).cpp
+++ 6.1 Brick drop (k bricks & n floor).cpp
@@ -1,67 +1,71 @@
 /*
-Problem Link: https://practice.geeksforgeeks.org/problems/egg-dropping-puzzle-1587115620/1
+Problem Link: https://leetcode.com/problems/super-egg-drop/
 
-Egg Dropping Puzzle 
+Super Egg Drop
 
-You are given N identical eggs and you have access to a K-floored building from 1 to K.
-There exists a floor f where 0 <= f <= K such that any egg dropped at a floor higher than f will break, and any egg dropped at or 
-below floor f will not break. There are few rules given below. 
+You are given k identical eggs and you have access to a building with n floors labeled from 1 to n. You know that there exists 
+a floor f where 0 <= f <= n such that any egg dropped at a floor higher than f will break, and any egg dropped at or below 
+floor f will not break.
 
-An egg that survives a fall can be used again. A broken egg must be discarded. The effect of a fall is the same for all eggs.
-If the egg doesn't break at a certain floor, it will not break at any floor below.
-If the eggs breaks at a certain floor, it will break at any floor above.
-Return the minimum number of moves that you need to determine with certainty what the value of f is.
+Each move, you may take an unbroken egg and drop it from any floor x (where 1 <= x <= n). If the egg breaks, you can no longer 
+use it. However, if the egg does not break, you may reuse it in future moves. Return the minimum number of moves that you need 
+to determine with certainty what the value of f is.
 
 Example 1:
-Input: N = 1, K = 2
+Input: k = 1, n = 2
 Output: 2
-Explanation: 
-1. Drop the egg from floor 1. If it  breaks, we know that f = 0.
-2. Otherwise, drop the egg from floor 2. If it breaks, we know that f = 1.
-3. If it does not break, then we know f = 2.
-4. Hence, we need at minimum 2 moves to determine with certainty what the value of f is.
-   
-Example 2:
-Input: N = 2, K = 10
-Output: 4
+Explanation: Drop the egg from floor 1. If it breaks, we know that f = 0. Otherwise, drop the egg from floor 2. If it breaks, we 
+know that f = 1. If it does not break, then we know f = 2.
+Hence, we need at minimum 2 moves to determine with certainty what the value of f is.
 
+Example 2:
+Input: k = 2, n = 6
+Output: 3
+Example 3:
+
+Input: k = 3, n = 14
+Output: 4
+ 
 Constraints:
-1 <= N <= 200
-1 <= K <= 200
+1 <= k <= 100
+1 <= n <= 10^4
 */
 
 class Solution{
 
-  public:
-  //find the critical floor.
-  int eggDrop(int n, int k){    // k = floor, n = bricks
+ public:
+   
+ int solver(vector<vector<int>>& dp, int n, int k){
+     if(k == 1) return n;
+     else if(n == 0 || n == 1) return n;
 
-      vector<vector<int>> dp(n+1, vector<int> (k+1));   // brick vs floor
+     if(dp[k][n] != -1) return dp[k][n]; 
 
-      //fill remaining cells
-      for(int i = 1; i <= n; i++){
-          for(int j = 1; j <= k; j++){
-              if(i == 1) dp[i][j] = j;
-              else if(j == 1) dp[i][j] = 1;
-              else{
-                  int mini = INT_MAX;
-                  int pj = 0;
-                  for(int nj = j-1; nj >= 0; nj--){
-                      int v1 = dp[i][nj];    //egg survives
-                      int v2 = dp[i-1][pj];  //egg breaks
-                      int val = max(v1, v2);
-                      mini = min(mini, val);
-                      pj++; //egg breaks
-                      int val = max(v1, v2);
-                      mini = min(mini, val);
-                      pj++;
-                  }
-                  dp[i][j] = mini+1;
-              }
-          }
-      }
-  return dp[n][k];
-  }
+     int l = 1, r = n;
+     int ans = 1e9;
+     while(l <= r){
+         int mid = (l+r)/2;
+
+         int cand1 = solver(dp, mid-1, k-1);    // broke (check below)
+         int cand2 = solver(dp, n-mid, k);      // unbroken (check above)
+
+         if(cand1 < cand2) l = mid+1;
+         else r = mid-1; 
+
+         ans = min(ans, max(cand1+1, cand2+1));
+     }
+
+     dp[k][n] = ans;
+    
+ return dp[k][n];
+ }
+
+ int superEggDrop(int k, int n) {
+
+     vector<vector<int>> dp(k+1, vector<int> (n+1, -1));   // brick vs floor
+     int ans = solver(dp, n, k);
+ return ans;  
+ }
 };
 
 
