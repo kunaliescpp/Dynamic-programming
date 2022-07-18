4054784171a19e2f735b764524a82ed9d0ecf6a1
kunal_cpp <kunaliesc@gmail.com>
2022-07-19 03:48:39 +1200

    Update Tri tiling.cpp

--- Tri tiling.cpp
+++ Tri tiling.cpp
@@ -1,1 +1,18 @@
+/*
+Problem Link: https://open.kattis.com/problems/tritiling
 
+In how many ways can you tile a 3xn rectangle with 2x1 dominoes?
+
+constraint:
+0 <= n <= 30
+
+Input: 
+testcases = 3
+2
+8
+12
+
+Output: 
+3
+153
+2131
